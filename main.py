f2a68c6fdbb75c89754740a7d35344e9ba8ff47e
ebgreenberg <ev.ben.green@gmail.com>
2019-11-18 10:22:27 -0800

    update with width finder

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pyproj import Proj
 from rivamap import preprocess, singularity_index, delineate, georef, visualization
 import rasterio
 from rasterio.plot import show
+from scipy.signal import argrelextrema
 from scipy.ndimage import label as ndlabel
 from scipy.ndimage import sum as ndsum
 
@@ -19,7 +20,7 @@ b6path = '/Users/evangreenberg/PhD Documents/Projects/river-profiles/Landsat/B6_
 DEMpath = '/Users/evangreenberg/PhD Documents/Projects/river-profiles/DEM/output_be.tif'
 
 
-def main(b3path, b6path, DEMpath): 
+def main(b3path, b6path, DEMpath, smoothing): 
 
     # Load the File
     print('Loading the files')
@@ -143,25 +144,174 @@ def main(b3path, b6path, DEMpath):
             xsections[idx[0]]['xsection'] = b
 
 
+#################################################
+####         FINDING CHANNEL WIDTH FOR ONE   ####
+#################################################
+# Method 1.: Use local minima and maxima, find the biggest difference between
+#            neighbors. Use the maximas as the channel endpoints
+# Method 2.: Use curvature, find places of maximum curvature (2nd deivative)
+def get_d1(test_section):
+    d1 = []
+    for idx in range(0, len(test_section)):
+        if idx == 0:
+            d = (
+                (test_section[idx+1][1] - test_section[idx][1])
+                / (test_section[idx+1][0] - test_section[idx][0])
+            )
+            d1.append(d)
+        elif idx == len(test_section) - 1:
+            d = (
+                (test_section[idx][1] - test_section[idx-1][1])
+                / (test_section[idx][0] - test_section[idx-1][0])
+            )
+            d1.append(d)
+        else:
+            d = (
+                (test_section[idx+1][1] - test_section[idx-1][1])
+                / (test_section[idx+1][0] - test_section[idx-1][0])
+            )
+            d1.append(d)
+
+    return d1
+
+def get_d2(test_section):
+    d2 = []
+    for idx in range(0, len(test_section)):
+        if idx == 0:
+            d = (
+                (test_section[idx+1][2] - test_section[idx][2])
+                / (test_section[idx+1][0] - test_section[idx][0])
+            )
+            d2.append(d)
+        elif idx == len(test_section) - 1:
+            d = (
+                (test_section[idx][2] - test_section[idx-1][2])
+                / (test_section[idx][0] - test_section[idx-1][0])
+            )
+            d2.append(d)
+        else:
+            d = (
+                (test_section[idx+1][2] - test_section[idx-1][2])
+                / (test_section[idx+1][0] - test_section[idx-1][0])
+            )
+            d2.append(d)
+
+    return d2
+
+
+# Get X Section
+idx = 1000 
+t = xsections[idx]['xsection']['demvalue_sm']
+p = xsections[idx]['xsection']['position']
+data = {'position': p, 'elevation': t}
+cross_section = pandas.DataFrame(data=data, columns=['position', 'elevation'])
+
+# Find Maxima and Minima
+order = 5 
+maxs = argrelextrema(t, np.greater, order=order)
+maxima = np.column_stack((p[maxs], t[maxs]))
+mins = argrelextrema(t, np.less, order=order)
+minima = np.column_stack((p[mins], t[mins]))
+
+extremes = np.concatenate([maxima, minima])
+extremes = extremes[extremes[:,0].argsort()]
+# Get biggest difference between ADJACENT maxima and minma
+d = []
+for i in range(0, len(extremes)):
+    if i == len(extremes) - 1:
+        d.append(0) 
+    else:
+        diff = extremes[i+1][1] - extremes[i][1]
+        d.append(diff)
+maxi = np.where(d == np.amax(d))[0][0]
+mini = np.where(d == np.amin(d))[0][0]
+max_val = extremes[maxi + 1]
+min_val = extremes[mini]
+# Take lowest of the two and project across the stream
+# This works because the opposite side of the channel HAS to have a different
+# Position sine. If that changes, this logic will have to change
+if min_val[1] >= max_val[1]:
+    opposite_channel_section = cross_section[
+        (cross_section['position'] > min_val[0]) 
+        & (cross_section['position'] < max_val[0])
+        & (cross_section['position'] < 0)
+    ]
+    width_val = max_val
+else:
+    opposite_channel_section = cross_section[
+        (cross_section['position'] > min_val[0]) 
+        & (cross_section['position'] < max_val[0])
+        & (cross_section['position'] > 0)
+    ]
+    width_val = min_val
+
+opposite_val = opposite_channel_section.iloc[
+    (
+        opposite_channel_section['elevation']
+        - width_val[1]
+    ).abs().argsort()[:1]
+].to_numpy()[0]
+
+# Width Val is the minimum of the adjacent max-mins
+# Opposite Val is the closes value on the opposite side of the channel
+
+# Plot
+plt.scatter(width_val[0], width_val[1], color='red')
+plt.scatter(opposite_val[0], opposite_val[1], color='red')
+plt.plot(p, t)
+plt.show()
+
+# Find first derivative of curve
+test_section = np.column_stack((p, t))
+test_section = np.column_stack((
+    test_section,
+    get_d1(test_section)
+))
+test_section = np.column_stack((
+    test_section,
+    get_d2(test_section)
+))
+
+# Plot
+fig, ax1 = plt.subplots()
+color = 'tab:red'
+plt.scatter(p[maxs], t[maxs], color='red')
+plt.scatter(p[mins], t[mins], color='green')
+ax1.plot(p, t, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = 'tab:blue'
+ax2.plot(p, test_section[:,2], color=color)
+ax2.plot(p, test_section[:,3], color='tab:green')
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
+#################################################
+####         MISC                            ####
+#################################################
+# To CSV
+index = 868 
+root = '~/PhD Documents/Projects/river-profiles/'
+fn = 'test_xsection_sm8.csv'
+test_array = xsections[index]['xsection']
+df = pandas.DataFrame(test_array)
+df.to_csv((root + fn))
+
+# From .npy
+root = '~/PhD Documents/Projects/river-profiles/'
+fn = 'xsections_test.npy'
+path = root + fn
+xsections = np.load(fn, allow_pickle=True)
+
 fig = plt.figure()
 ax1 = plt.plot(df_sm['position'], df_sm['demvalue'])
 plt.plot(x, y, linestyle='dashed')
 plt.show()
 
-np.save('xsections_test', xsections)
-
-
-#################################################
-####         MISC                            ####
-#################################################
-# To CSV
-index = 0 
-root = '~/PhD Documents/Projects/river-profiles/'
-fn = 'test_xsection_sm6.csv'
-test_array = xsections[index]['xsection']
-df = pandas.DataFrame(df_x)
-df.to_csv((root + fn))
-
+np.save(root + 'xsections_test', xsections)
 
 #################################################
 ####         NEXT STEPS                      ####
@@ -185,37 +335,3 @@ df.to_csv((root + fn))
 # # plt.show()
 # 
 # 
-# #################################################
-# ####         PLOTTING                        ####
-# #################################################
-# river_mask_d = np.ma.masked_where(
-#     river_dem <= 0.,
-#     river_dem 
-# )
-# river_mask = np.ma.masked_where(
-#     centerline == False,
-#     centerline 
-# )
-# 
-# x, y = dem_clean.shape
-# extent = 0, y, 0, x 
-# fig = plt.figure(frameon=False)
-# 
-# im1 = plt.imshow(
-#     dem_clean, 
-#     cmap=plt.cm.gray, 
-#     norm=colors.Normalize(),
-#     interpolation='nearest', 
-#     extent=extent
-# )
-# # Set up a colormap:
-# # use copy so that we do not mutate the global colormap instance
-# im2 = plt.imshow(
-#     river_mask_d,
-#     cmap=plt.cm.plasma, 
-#     norm=colors.Normalize(),
-#     interpolation='bilinear',
-#     extent=extent
-# )
-# 
-# plt.show()
